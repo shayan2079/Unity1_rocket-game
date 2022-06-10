403b41be91f31a3783a8027bc535cedbc9e265f9
shayan2079 <shahshayan2079@gmail.com>
2022-06-11 00:36:42 +0430

    Sound effect and moving ability added to the rocket.

---
+++ Assets/Rocket.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            if (audioSource.isPlaying)
+            {
+                audioSource.Stop();
+            }
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(Vector3.right);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(Vector3.left);
+        }
+    }
+}
